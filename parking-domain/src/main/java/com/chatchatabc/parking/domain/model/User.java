3e2e2e87be1a858f868d8e8a433f5a92f1ed8304
Libing Chen <libing.chen@gmail.com>
2023-04-11 16:14:52 +0800

    ddd(model): User entity

---
+++ parking-domain/src/main/java/com/chatchatabc/parking/domain/model/User.java
@@ -0,0 +1,60 @@
+package com.chatchatabc.parking.domain.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private Long id;
+    private String realName;
+    private String phone;
+    private String driverLicense;
+    private int flag;
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDriverLicense() {
+        return driverLicense;
+    }
+
+    public void setDriverLicense(String driverLicense) {
+        this.driverLicense = driverLicense;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
